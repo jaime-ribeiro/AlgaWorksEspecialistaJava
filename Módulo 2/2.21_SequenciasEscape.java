d7e245a366cf8e2fe748f77736042b612f98a0e0
Jaime Ribeiro <littlejou@gmail.com>
2023-10-30 11:35:58 -0300

    atualizando comentários do exercício 2.21

--- Módulo 2/2.21_SequenciasEscape.java
+++ Módulo 2/2.21_SequenciasEscape.java
@@ -11,9 +11,15 @@ public class FormatandoComPrintf {
     System.out.printf("A quantidade é: %d itens%n", quantidade);
 
     //Para pontos flutuantes, se usa o %f
+    
     //Por padrão ele adiciona 6 casas decimais, para colocar somente 2 casas decimais, precisa colocar .2 antes do f, ficando assim: "%.2f"
+    
+    // O número 10 antes do "." indica o tamanho de caracteres que deve ter, contando a precisão e o "." do valor,  exemplo.: %10.2f%n
+    
+    //Caso tenha espaços a menos, ele colocará espaços em branco " " para preencher, por exemplo o peso, ficará: ____938.22, entretanto ao invés de underline, serão espaços em branco.
+
     double peso = 938.22;
     System.out.printf("Peso: %.2f%n",peso);
-    
+    //Dependendo das configurações de linguagem, o separador decimal pode estar "," ao invés de ".", em português aparece "," e inglês aparece ".".
   } 
 }
